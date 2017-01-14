f93537442a1c6f7b540ff2bbc64ef57620cc8b75
aliceinnets <aliceinnets@gmail.com>
2017-01-14 19:42:10 +0100

    added stopwatch and countword methods in oneliners

---
+++ src/util/OneLiners.java
@@ -0,0 +1,107 @@
+package util;
+import java.util.Scanner;
+
+public class OneLiners {
+	
+	
+	public static int countWordUsingScanner(String document, String word) {
+		return countWordUsingScanner(document, word, false, false, false);
+	}
+	
+	
+	public static int countWordUsingScanner(String document, String word, boolean caseSensitive) {
+		return countWordUsingScanner(document, word, caseSensitive, false, false);
+	}
+	
+	
+	public static int countWordUsingScanner(String document, String word, boolean caseSensitive, boolean wholeWord) {
+		return countWordUsingScanner(document, word, caseSensitive, wholeWord, false);
+	}
+	
+	/**
+	 * Counting the word in the document string using scanner. It is slower than {@link #countWord} method.
+	 * 
+	 * @param document
+	 * @param word
+	 * @param caseSensitive
+	 * @param wholeWord
+	 * @param excludeNonWordChars
+	 * @return number of occurrences of the word in the document
+	 */
+	public static int countWordUsingScanner(String document, String word, boolean caseSensitive, boolean wholeWord, boolean excludeNonWordChars) {
+		int count = 0;
+		
+		if(!caseSensitive) document = document.toLowerCase();
+		if(excludeNonWordChars) word = word.replaceAll("\\W", "");
+		
+		Scanner scanner = new Scanner(document);
+		if(wholeWord) {
+			if(excludeNonWordChars) {
+				while(scanner.hasNext()) if(scanner.next().equals(word)) ++count;
+			} else {
+				while(scanner.hasNext()) if(scanner.next().replaceAll("\\W", "").equals(word)) ++count;
+			}
+		} else {
+			if(excludeNonWordChars) {
+				while(scanner.hasNext()) if(scanner.next().contains(word)) ++count;
+			} else {
+				while(scanner.hasNext()) if(scanner.next().replaceAll("\\W", "").contains(word)) ++count;
+			}
+			
+		}
+		scanner.close();
+		
+		return count;
+	}
+	
+	
+	public static int countWord(String document, String word) {
+		return countWord(document, word, false, false, false);
+	}
+	
+	
+	public static int countWord(String document, String word, boolean caseSensitive) {
+		return countWord(document, word, caseSensitive, false, false);
+	}
+	
+	
+	public static int countWord(String document, String word, boolean caseSensitive, boolean wholeWord) {
+		return countWord(document, word, caseSensitive, wholeWord, false);
+	}
+	
+	/**
+	 * Counting the word in the document string.
+	 * 
+	 * @param document
+	 * @param word
+	 * @param caseSensitive
+	 * @param wholeWord
+	 * @param excludeNonWordChars
+	 * @return number of occurrences of the word in the document
+	 */
+	public static int countWord(String document, String word, boolean caseSensitive, boolean wholeWord, boolean excludeNonWordChars) {
+		int count = 0;
+		
+		if(!caseSensitive) document = document.toLowerCase();
+		if(excludeNonWordChars) word = word.replaceAll("\\W", "");
+		
+		String[] words = document.split("\\s+");
+		if(wholeWord) {
+			if(excludeNonWordChars) {
+				for(int i=0;i<words.length;++i) if(words[i].equals(word)) ++count;
+			} else {
+				for(int i=0;i<words.length;++i) if(words[i].replaceAll("\\W", "").equals(word)) ++count;
+			}
+		} else {
+			if(excludeNonWordChars) {
+				for(int i=0;i<words.length;++i) if(words[i].contains(word)) ++count;
+			} else {
+				for(int i=0;i<words.length;++i) if(words[i].replaceAll("\\W", "").contains(word)) ++count;
+			}
+			
+		}
+		
+		return count;
+	}
+
+}
