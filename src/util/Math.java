e9d78493a31ef84b15ed2300e101530d95dea7bb
aliceinnets <aliceinnets@gmail.com>
2017-01-17 19:06:04 +0100

    added Math class and method for generating binary method for 2D array from the original binary method for non-array parameters

---
+++ src/util/Math.java
@@ -0,0 +1,21 @@
+package util;
+
+public class Math {
+	
+	public static long add(long a, long b) {
+        return a+b;
+    }
+
+    public static long subtract(long a, long b) {
+        return a-b;
+    }
+
+    public static long mul(long a, long b) {
+        return a*b;
+    }
+
+    public static long divide(long a, long b) {
+        return a/b;
+    }
+
+}
