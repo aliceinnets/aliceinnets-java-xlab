98d0e12dda08faa81a17459f3652c6cd4e88de20
aliceinnets <aliceinnets@gmail.com>
2017-02-07 13:19:38 +0100

    played with pattern

---
+++ src/aliceinnets/xlab/PlayWithPattern.java
@@ -0,0 +1,23 @@
+package aliceinnets.xlab;
+
+public class PlayWithPattern {
+	
+	public static void main(String[] args) {
+		String a = "afsdg[10352]";
+		String b = "age2_3[1054265]";
+		
+		System.out.println(isArray(a));
+		System.out.println(isArray(b));
+		
+	}
+	
+	
+	public static boolean isArray(String name) {
+		if(name.matches(".+\\[\\d+\\]")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
