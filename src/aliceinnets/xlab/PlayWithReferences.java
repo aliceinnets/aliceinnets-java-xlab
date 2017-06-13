0bc88d9add422b197422e26f9d630e13c348a640
aliceinnets <aliceinnets@gmail.com>
2017-06-13 21:59:15 +0100

    played with reference

--- src/aliceinnets/xlab/PlayWithReferences.java
+++ src/aliceinnets/xlab/PlayWithReferences.java
@@ -2,6 +2,8 @@ package aliceinnets.xlab;
 
 import java.util.Arrays;
 
+import aliceinnets.util.OneLiners;
+
 public class PlayWithReferences {
 	
 	public static void main(String[] args) {
@@ -9,7 +11,32 @@ public class PlayWithReferences {
 		DoubleArray b = new DoubleArray();
 		b.setArray(a);
 		
-		System.out.println(Arrays.toString(a));
+		OneLiners.println(a);
+		
+		changeElement(a, 2.0);
+		OneLiners.println(a);
+		changeElement(a, 3.0);
+		OneLiners.println(a);
+		changeElement(a, 4.0);
+		OneLiners.println(a);
+		
+		Double c = new Double(0.0);
+		changeObject(c, 1.0);
+		System.out.println(c);
+	}
+	
+	public static void changeElement(double[] a, double b) {
+		if(a.length > 0) {
+			a[0] = b;
+		}
+	}
+	
+	public static void changeDouble(Double a, double b) {
+		a = b;
+	}
+	
+	public static void changeObject(Object a, Object b) {
+		a = b;
 	}
 	
 	public static class DoubleArray {
