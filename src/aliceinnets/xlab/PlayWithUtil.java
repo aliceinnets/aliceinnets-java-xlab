83590eadf20323a06a91eb1628a0542d01a0f166
aliceinnets <aliceinnets@gmail.com>
2017-06-22 01:16:56 +0200

    played with util

---
+++ src/aliceinnets/xlab/PlayWithUtil.java
@@ -0,0 +1,25 @@
+package aliceinnets.xlab;
+
+import aliceinnets.maven.util.MavenUtil;
+import aliceinnets.util.OneLiners;
+
+public class PlayWithUtil {
+	
+	public static void main(String[] args) {
+		PlayWithUtil util = new PlayWithUtil();
+		util.getPrintUserDir();
+		
+		System.out.println(MavenUtil.getModel().getArtifactId());
+		System.out.println(MavenUtil.getModel().getGroupId());
+		System.out.println(MavenUtil.getModel().getVersion());
+	}
+	
+	public PlayWithUtil() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void getPrintUserDir() {
+		OneLiners.printUserDir();
+	}
+
+}
