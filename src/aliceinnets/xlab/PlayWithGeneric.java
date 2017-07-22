086eafcf5f14a9f5209cedc6440983c72da3677c
aliceinnets <aliceinnets@gmail.com>
2017-07-22 13:51:51 +0200

    played with generic

---
+++ src/aliceinnets/xlab/PlayWithGeneric.java
@@ -0,0 +1,22 @@
+package aliceinnets.xlab;
+
+public class PlayWithGeneric {
+	
+	public static void main(String[] args) {
+		int a = 1;
+		double b = cast(a);
+		System.out.println(b);
+	}
+	
+	public final static <T> int cast(T obj) {
+		return (int) obj;
+	}
+	
+	public final static <T> T cast(Object obj, Class<?> type) {
+		if(type.equals(double.class)) {
+//			return (T) Double.parseDouble(obj.toString());
+		}
+		return null;
+	}
+
+}
